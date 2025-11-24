c59f3874cbd04a7755620160e3cce459bb6cbbe5
yuldashev7 <yfaxriddin540@gmail.com>
2025-11-24 16:34:15 +0500

    vercel

--- server.js
+++ server.js
@@ -1,8 +1,9 @@
+const path = require('path');
 const jsonServer = require('json-server');
 const auth = require('json-server-auth');
 
 const server = jsonServer.create();
-const router = jsonServer.router('db.json');
+const router = jsonServer.router(path.join(__dirname, 'db.json'));
 const middlewares = jsonServer.defaults();
 
 server.use(middlewares);
